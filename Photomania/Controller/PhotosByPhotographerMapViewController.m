1bb1d52fc805d63c9811580cee4e8f816c0e8488
Neo Lee <neo.lee@gmail.com>
2014-09-11 12:40:35 +0800

    Map on iPhone done

--- Photomania/Controller/PhotosByPhotographerMapViewController.m
+++ Photomania/Controller/PhotosByPhotographerMapViewController.m
@@ -9,6 +9,7 @@
 #import "PhotosByPhotographerMapViewController.h"
 #import <MapKit/MapKit.h>
 #import "Photo+Flickr.h"
+#import "ImageViewController.h"
 
 @interface PhotosByPhotographerMapViewController () <MKMapViewDelegate>
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
@@ -46,6 +47,76 @@
     }
 
     return _photosByPhotographer;
+}
+
+#pragma mark - Map view delegate
+
+- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation {
+    static NSString *reuseIdentifier = @"Photo by Photographer";
+    MKAnnotationView *view = [mapView dequeueReusableAnnotationViewWithIdentifier:reuseIdentifier];
+    if (!view) {
+        view = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:reuseIdentifier];
+        view.canShowCallout = YES;
+
+        UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 46, 46)];
+        view.leftCalloutAccessoryView = imageView;
+
+        view.rightCalloutAccessoryView = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
+    }
+
+    view.annotation = annotation;
+
+    return view;
+}
+
+- (void)mapView:(MKMapView *)mapView didSelectAnnotationView:(MKAnnotationView *)view {
+    [self updateLeftCalloutAccessoryViewInAnnotationView:view];
+}
+
+- (void)mapView:(MKMapView *)mapView annotationView:(MKAnnotationView *)view calloutAccessoryControlTapped:(UIControl *)control {
+    [self performSegueWithIdentifier:@"Show Photo" sender:view];
+}
+
+- (void)updateLeftCalloutAccessoryViewInAnnotationView:(MKAnnotationView *)annotationView {
+    UIImageView *imageView = nil;
+    if ([annotationView.leftCalloutAccessoryView isKindOfClass:[UIImageView class]]) {
+        imageView = (UIImageView *)annotationView.leftCalloutAccessoryView;
+    }
+    if (imageView) {
+        Photo *photo = nil;
+        if ([annotationView.annotation isKindOfClass:[Photo class]]) {
+            photo = (Photo *)annotationView.annotation;
+        }
+        if (photo) {
+            imageView.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:photo.thumbnailURL]]];
+        }
+    }
+}
+
+#pragma mark - Navigation
+
+- (void)prepareViewController:(id)vc forSegueWithIdentifier:(NSString *)identifier withAnnotation:(id <MKAnnotation>)annotation {
+    Photo *photo = nil;
+    if ([annotation isKindOfClass:[Photo class]]) {
+        photo = (Photo *)annotation;
+    }
+    if (photo) {
+        if (![identifier length] || [identifier isEqualToString:@"Show Photo"]) {
+            if ([vc isKindOfClass:[ImageViewController class]]) {
+                ImageViewController *imageViewControll = (ImageViewController *)vc;
+                imageViewControll.title = photo.title;
+                imageViewControll.imageURL = [NSURL URLWithString:photo.imageURL];
+            }
+        }
+    }
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    if ([sender isKindOfClass:[MKAnnotationView class]]) {
+        [self prepareViewController:segue.destinationViewController
+             forSegueWithIdentifier:segue.identifier
+                     withAnnotation:((MKAnnotationView *)sender).annotation];
+    }
 }
 
 @end
